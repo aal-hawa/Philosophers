3dc9276482719bfc2f23ae1e12a44614191c6473
Anas Al Hawamda <aal-hawa@lab3r4s7.42abudhabi.ae>
2024-11-20 18:51:54 +0400

    edit

--- philo/threads/routine.c
+++ philo/threads/routine.c
@@ -6,7 +6,7 @@
 /*   By: aal-hawa <aal-hawa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 16:13:04 by aal-hawa          #+#    #+#             */
-/*   Updated: 2024/11/20 18:50:50 by aal-hawa         ###   ########.fr       */
+/*   Updated: 2024/11/20 18:51:50 by aal-hawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -162,7 +162,7 @@ void *do_threed_philo(void *ptr)
 	parm = (t_parm *)ptr;
 	this_time = 0;
 	philo = NULL;
-	select_philo_fork(parm, philo, parm->info);
+	philo = select_philo_fork(parm, philo, parm->info);
 	loop_philo(parm, philo, parm->info, this_time);
 	return (NULL);
 }
