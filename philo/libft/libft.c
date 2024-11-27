2e8c139005be7ad256d5e39147b4da25ad085b02
aal-hawa <aal-hawa@student.42abudhabi.ae>
2024-11-27 15:44:44 +0400

    edit

--- philo/libft/libft.c
+++ philo/libft/libft.c
@@ -6,7 +6,7 @@
 /*   By: aal-hawa <aal-hawa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 16:36:58 by aal-hawa          #+#    #+#             */
-/*   Updated: 2024/11/25 18:13:16 by aal-hawa         ###   ########.fr       */
+/*   Updated: 2024/11/27 15:30:36 by aal-hawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,14 @@ int	check_is_char(char *str)
 		else
 		{
 			if (str[i] == '-')
-				error_massege("Error nigative number", 0);
+				error_massege("Error parsing", 0);
 			else if (i == 0 && str[i] == '+')
 			{
 				i++;
 				continue ;
 			}
 			else
-				error_massege("Error only numbers", 0);
+				error_massege("Error parsing", 0);
 			return (-1);
 		}
 	}
--- philo/main_mandotiry.c
+++ philo/main_mandotiry.c
@@ -6,7 +6,7 @@
 /*   By: aal-hawa <aal-hawa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 16:08:49 by aal-hawa          #+#    #+#             */
-/*   Updated: 2024/11/26 11:11:24 by aal-hawa         ###   ########.fr       */
+/*   Updated: 2024/11/27 15:30:59 by aal-hawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	check_args(t_info *info, int ac, char **arg, int is_error)
 	{
 		info->how_many_eat = ft_atoi(arg[5]);
 		if (info->how_many_eat == 0)
-			is_error = error_massege("Error no philo", is_error);
+			is_error = error_massege("Error parsing", is_error);
 	}
 	else
 		info->how_many_eat = 0;
@@ -32,16 +32,16 @@ int	init_info(t_info *info, int ac, char **arg)
 	is_error = 0;
 	info->philo_count = ft_atoi(arg[1]);
 	if (info->philo_count == 0)
-		is_error = error_massege("Error no philo", is_error);
+		is_error = error_massege("Error parsing", is_error);
 	info->to_die = ft_atoi(arg[2]);
 	if (info->to_die == 0)
-		is_error = error_massege("Error no philo", is_error);
+		is_error = error_massege("Error parsing", is_error);
 	info->to_eat = ft_atoi(arg[3]);
 	if (info->to_eat == 0)
-		is_error = error_massege("Error no philo", is_error);
+		is_error = error_massege("Error parsing", is_error);
 	info->to_sleep = ft_atoi(arg[4]);
 	if (info->to_sleep == 0)
-		is_error = error_massege("Error no philo", is_error);
+		is_error = error_massege("Error parsing", is_error);
 	is_error = check_args(info, ac, arg, is_error);
 	info->last_philo_got_it = 1;
 	info->is_get_timer = 0;
@@ -57,7 +57,7 @@ int	main(int ac, char **arg)
 
 	if (ac < 5 || ac > 6)
 	{
-		error_massege("Error four or five paramiter", 0);
+		error_massege("Error parsing", 0);
 		return (1);
 	}
 	is_error = parsing(ac, arg);
